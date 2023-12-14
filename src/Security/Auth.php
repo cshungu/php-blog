df3549148a27c9f2a1f155e2c1737ef722579fa2
Christian Shungu <christianshungu@gmail.com>
2023-12-14 18:48:00 +0100

    Ajout du Fichier

--- src/Security/Auth.php
+++ src/Security/Auth.php
@@ -2,7 +2,6 @@
 
 namespace App\Security;
 
-
 /**
  * Class Auth
  * 
@@ -16,4 +15,74 @@ namespace App\Security;
  */
 class Auth
 {
+    /**
+     * Method user
+     * 
+     * @return User
+     */
+    public function user()
+    {
+        // if (isset($_SESSION['user'])) {
+        //     return Users::find($_SESSION['user']);
+        // }
+    }
+    /**
+     * Method check
+     * 
+     * @return mixed 
+     */
+    public function check()
+    {
+        return isset($_SESSION['user']);
+    }
+    /**
+     * Method attempt
+     * 
+     * @param string|null $email    - Adresse mail
+     * @param string|null $password - Mot de passe
+     * 
+     * @return boolean
+     */
+    public function attempt($email, $password)
+    {
+        // $user = Users::where('email', $email)
+        //     ->orWhere('login', '=', $email)->first();
+        // if (!$user) {
+        //     return false;
+        // }
+        // if (password_verify($password, $user->password)) {
+        //     $_SESSION['user'] = $user->id;
+        //     return true;
+        // }
+        // return false;
+    }
+    /**
+     * Method Deconnecter
+     * 
+     * @return mixed
+     */
+    public function deconnecter()
+    {
+        $_SESSION = [];
+        session_destroy();
+        unset($_SESSION);
+    }
+    /**
+     * Method isExist
+     * 
+     * @param string|null $email - Adresse mail
+     *
+     * @return boolean
+     */
+    public function isExist($email)
+    {
+        // // $user = Users::where(
+        // //     'email',
+        // //     $email
+        // // )->first(); 
+        // if (!$user) {
+        //     return false;
+        // }
+        return true;
+    }
 }
